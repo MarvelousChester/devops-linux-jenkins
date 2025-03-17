014471943e9651ff94f4872a2e1a3cf37c5cff92
Karandeep Sandhu <90730377+MarvelousChester@users.noreply.github.com>
2025-03-17 16:04:16 -0400

    CORE-1549 Test: file path existence using shell script

--- groovy/unityHelper.groovy
+++ groovy/unityHelper.groovy
@@ -256,17 +256,26 @@ int runUnityBatchMode(String unityExecutable, String projectDirectory, String re
 void validateBuildLightingFiles(){
 
     echo 'Finding Lighting and Probe files...'
+    
+    // Test using sh
+    def fileExists = sh(script: "[ -f '${env.PROJECT_DIR}/Assets/Scenes/Main Scene/LightingData.asset' ] && echo 'true' || echo 'false'", returnStdout: true).trim() == 'true'
+    if (fileExists) {
+        echo "File exists!"
+    } else {
+        echo "File does not exist."
+    }
+
 
     warnError('Lighting file NOT found'){
-        def filePath = "\"${PROJECT_DIR}/Assets/Scenes/Main Scene/LightingData.asset\""
+        def filePath = "${env.PROJECT_DIR}/Assets/Scenes/Main\\Scene/LightingData.asset"
         if(!fileExists(filePath)){
             error("File does not exist at: ${filePath}")
         }
         echo "Lighting file found"
     }
-    warnError('Reflection Probe Lighting file NOT found'){
-        def filePath = "\"${PROJECT_DIR}/Assets/Scenes/Main Scene/ReflectionProbe-0.exr\""
-        if(fileExists("\"${env.PROJECT_DIR}/Assets/Scenes/Main Scene/ReflectionProbe-0.exr\"")){
+    warnError('Reflection Probe Lighting file NOT found'){ // QUESTION, is this needed?
+        def filePath = "${env.PROJECT_DIR}/Assets/Scenes/Main\\Scene/ReflectionProbe-0.exr"
+        if(!fileExists(filePath)){
             error("File does not exist at: ${filePath}")
         }
         echo "Reflection Probe Lighting file found"
